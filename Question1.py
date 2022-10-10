658ad25b2db1a647ef577491dde3ba431ff37276
Vasanth Seethapathi <114441240+Vasanth2796@users.noreply.github.com>
2022-10-10 18:07:09 -0400

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,19 @@
+def solution(list, num): 
+    a=0 
+    b=0 
+    '''type in your solution, find a and b in array that a+b=num'''
+    # print(numbers)
+    vals = {}
+    for i, val in enumerate(numbers):
+        if num - val in vals:
+            a = num-val
+            b = numbers[i]
+            break
+        else:
+            vals[val] = i
+    return a, b 
+  
+numbers = [0, 21, 78, 19, 90, 13] 
+print(solution(numbers, 21))
+print(solution(numbers, 25))
+print(solution(numbers, 103))
